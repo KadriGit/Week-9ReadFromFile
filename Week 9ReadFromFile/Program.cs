8e7561210fe444b6370070200b50d7b14b8a4770
KadriGit <kadri.viira@tktk.ee>
2024-11-15 10:31:30 +0200

    first part

--- Week 9ReadFromFile/Program.cs
+++ Week 9ReadFromFile/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string fileDrirectoryPath = @"C:\Users\Lenovo\Desktop\data";
+string fileName = "chuck.txt";
+
+string fullFilePath = Path.Combine(fileDrirectoryPath, fileName);
+
+if (File.Exists(fullFilePath))
+{
+    Console.WriteLine($"File{fileName} exists in {fileName}");
+}
+else
+{
+    Console.WriteLine($"File{fileName} was not found");
+}
