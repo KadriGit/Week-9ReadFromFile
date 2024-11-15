742f31b4ca9a5ec438a4db410212409cd0792c41
KadriGit <kadri.viira@tktk.ee>
2024-11-15 10:37:48 +0200

    second part

--- Week 9ReadFromFile/Program.cs
+++ Week 9ReadFromFile/Program.cs
@@ -7,6 +7,13 @@ string fullFilePath = Path.Combine(fileDrirectoryPath, fileName);
 if (File.Exists(fullFilePath))
 {
     Console.WriteLine($"File{fileName} exists in {fileName}");
+   String[] chuckJokes =  File.ReadAllLines(fullFilePath);
+    Console.WriteLine($"There are {chuckJokes.Length} jokes about chuck Norris in the filr.");
+
+    foreach(string joke in chuckJokes)
+    {
+        Console.WriteLine(joke);
+    }
 }
 else
 {
